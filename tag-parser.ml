22fed0bd0852973b3218f6ec68ee2a69f5fd409c
ode dbasaraba <odedbasaraba08@gmail.com>
2020-11-30 19:16:47 +0200

    done till lambda

--- tag-parser.ml
+++ tag-parser.ml
@@ -58,8 +58,76 @@ let reserved_word_list =
 
 (* work on the tag parser starts here *)
 
-let tag_parse_expressions sexpr = raise X_not_yet_implemented;;
+let rec tag_parse = function 
+  (* |Pair(Symbol("if"), Pair(test, Pair(dit, Pair(dif, Nil))))->If(tag_parse test, tag_parse dit, tag_parse dif) *)
+  |Pair(Symbol("quote"), Pair(x, Nil)) -> Const(Sexpr(x))
+  |Number(x) -> Const(Sexpr(Number(x)))
+  |String(x) -> Const(Sexpr(String(x)))
+  |Char(x) -> Const(Sexpr(Char(x)))
+  |Bool(x) -> Const(Sexpr(Bool(x)))
+  |Symbol(x) -> 
+                (if (List.exists (fun v-> x = v) reserved_word_list) then raise X_syntax_error
+                 else  Var x)
+  |Pair(Symbol("if"), Pair(test, Pair(dit, Pair(dif, Nil)))) -> If(tag_parse test, tag_parse dit, tag_parse dif)  (* if test dit dif *)
+  |Pair(Symbol("if"), Pair(test, Pair(dit, Nil))) -> If(tag_parse test, tag_parse dit, Const(Void))              (* if test then *)
+  |Pair(Symbol ("lambda"), Pair(arglist, Pair( exp, Nil))) -> parsing_lambda (Pair (arglist,exp))
 
-  
+  and proper_list = function                    (*check the list for the lambda*)
+    |Nil -> true 
+    |Pair (l,r) -> proper_list(r)
+    |_ ->false
+    
+  and convert_to_list =function
+  | Pair(l,r)-> l :: (convert_to_list r)
+  | Nil -> []
+  | l -> [l]
+
+  and parsing_lambda  = function 
+  |(Pair (arglist,exp))->
+                if (proper_list arglist) then (make_simple_lambda (Pair (arglist,exp))) 
+                else (make_opt_lambda(Pair (arglist,exp)))
+  |_ -> raise X_syntax_error
+        (* parsing the sequence *)
+  and sequence tmp_body= 
+      match (List.length tmp_body) with 
+      |0-> Const(Void)
+      |1 ->  tag_parse (List.hd tmp_body)
+      |_ ->  Seq (List.map (fun a-> (tag_parse a)) tmp_body)
+    (* check for duplicate in the are list  *)
+  and check_dup args =
+      match args with 
+      |[] -> false
+      | head::tail -> List.exists (fun(element)->element = head) tail || check_dup tail
+        (* geting the value of symbol *)
+  and remSym sym= 
+        match sym with 
+                 | Symbol(a)-> a
+                 | _->raise X_syntax_error
+    (* simple lambda *)
+  and make_simple_lambda = function
+  |(Pair (l,r))->
+                  let tmp_args = (convert_to_list l) in 
+                  let args = (List.map remSym tmp_args) in 
+                  let tmp_body = (convert_to_list r) in 
+                  let body =  (sequence tmp_body) in 
+                  if body= Const(Void) || (check_dup args) then raise X_syntax_error
+                  else LambdaSimple (args, body)
+  |_ -> raise X_syntax_error
+    (* opt lambda *)
+  and make_opt_lambda = function
+  |(Pair (l,r))->
+                  let tmp_args = (convert_to_list l) in 
+                  let args = (List.map remSym tmp_args) in 
+                  let last_arg =  (List.nth args ((List.length args) - 1)) in    (*we know its an improper list *)
+                  let tmp_body = (convert_to_list r) in 
+                  let body =  (sequence tmp_body) in
+                  if body= Const(Void) || (check_dup args) then raise X_syntax_error
+                  else LambdaOpt (args, last_arg, body)
+  |_ -> raise X_syntax_error
+;;
+
+
+let tag_parse_expression sexpr = tag_parse sexpr;;
+let tag_parse_expressions sexpr = List.map tag_parse sexpr;;
 end;; (* struct Tag_Parser *)
 
